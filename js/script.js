2ca3e1d569fa2b1f2c3cd690da44a6c1220f038b
HarpritKaurSidhu <sidhuharprit8@gmail.com>
2022-05-12 18:29:28 -0400

    script changed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 console.log(window.navigator);
 if ('serviceWorker' in navigator) {
 
-    navigator.serviceWorker.register('../service-worker.js',{scope:'/pwa-test/'});
+    navigator.serviceWorker.register('/pwa-test/service-worker.js',{scope:'/pwa-test/'})
+    .then(function(){
+
+        console.log('SW ok');
+    });
 }
